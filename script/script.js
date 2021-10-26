469589c8cd2d925ebe4d67e92d27e929283c0fde
Sushant Patial <sushantpatial18@gmai.com>
2021-10-26 11:54:03 +0530

    Made changes

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
+// Initializing AOS
 AOS.init();
 
+// Reload on top
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
+// Opening and closing side nav for small screens
 function openNav() {
   $("#side-nav").css("width", "250px");
 }
@@ -12,17 +15,19 @@ function closeNav() {
   $("#side-nav").css("width", "0px");
 }
 
+// Navbar background colour when scrolled
 $(window).scroll(function() {    
   var scroll = $(window).scrollTop();
 
   if (scroll >= 500) {
-      $(".navbar").addClass("navbar-dark");
+      $(".navbar").addClass("navbar-bg");
   }
   else {
-    $(".navbar").removeClass("navbar-dark");
-}
+    $(".navbar").removeClass("navbar-bg");
+  }
 });
 
+// Hover for social icons
 function hover(element) {
   element.setAttribute('src', 'images/icons/hover/' + element.className + '.png');
 };
@@ -31,9 +36,7 @@ function unhover(element) {
   element.setAttribute('src', 'images/icons/' + element.className + '.png');
 };
 
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
+// Scroll to top button
 $(window).scroll(function() {
   if ($(this).scrollTop() > 100) {
     $('#top-btn').fadeIn();
@@ -42,9 +45,48 @@ $(window).scroll(function() {
   }
 });
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   $("html, body").animate({
     scrollTop: 0
   }, 100);
+}
+
+// Adding active class on scroll
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $('a').each(function () {
+          $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+
+      var target = this.hash,
+          menu = target;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
+  });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.navbar-nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.navbar-nav ul li').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
 }
