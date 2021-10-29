224d540b71b904e988de0e2943cdfa525bd967a2
Sushant Patial <sushantpatial18@gmai.com>
2021-10-29 19:38:32 +0530

    Fix headers

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+  $link = mysqli_connect("sdb-m.hosting.stackcp.net", "eazyerp-313935e8ca", "sushant123", "eazyerp-313935e8ca");
+
+  if (mysqli_connect_error())
+  {
+    die ("Database connection error. Please try again later.");
+  }
+?>
